b1bd43d6c247261b322a09890cb347b4e6d90aff
liuyanjie <lyj8888888888@gmail.com>
2019-07-19 19:49:32 +0800

    Release 1.1.3

--- tests/Config.php
+++ tests/Config.php
@@ -9,8 +9,8 @@ class Config
     public static $endpoint = "tcb.tencentcloudapi.com";
     public static $version = "2018-06-08";
 
-    public static $secretId = "AKIDORiMgDgJrLPjxvKDm9F77NGixduHpm0o";
-    public static $secretKey = "JcFUANtb3cLtlbMuwZN9y6MIyMVXBPDG";
+    public static $secretId = "";
+    public static $secretKey = "";
 
     public static $appId = "";
     public static $region = "";
